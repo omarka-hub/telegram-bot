6400bff2aa769097a4a65ae315ef6395d25bca5e
omarka-hub <omaralkreal07@gmail.com>
2025-09-14 03:24:38 -1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from script import quiz_bot
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Flask will look in templates/
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    questions = request.form.get('questions')
+    status = quiz_bot(questions)
+    return status
+
+if __name__ == "__main__":
+    app.run(debug=True)
