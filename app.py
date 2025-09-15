495b7b392c7c1d393e6825fd1b1f34147ea83bd8
omarka-hub <omaralkreal07@gmail.com>
2025-09-14 20:53:33 -1200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request
-from script import quiz_bot
+from script import quiz_bot, send_message
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Flask will look in templates/
+    return render_template('index.html')  
 
 @app.route('/submit', methods=['POST'])
 def submit():
     questions = request.form.get('questions')
+    topic = request.form.get('topic')
+    #message = send_message(topic)
     status = quiz_bot(questions)
     return status
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
-#2025
